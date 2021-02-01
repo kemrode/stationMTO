62760cf517559890d8828a4e9c81c6668ae37a0c
Kevin Fichou <kevinfichou@gmail.com>
2021-02-01 22:53:19 +0100

    <create>303:append function to hide and display the rollOut menu

--- scriptJavaS.js
+++ scriptJavaS.js
@@ -1,2 +1,22 @@
-let menuBtn = document.querySelector('menuPicture');
+let menuBtn = document.querySelector('menuButton');
+let primaryMenu = document.getElementById('#menuRollOut');
 
+let menuHidden=true;
+
+primaryMenu = document.style.display="none";
+document.getElementById('menuRollOut').style.display='none';
+
+menuBtn.addListener('submit', ()=> {
+    if(menuHidden==false){
+        primaryMenu = document.style.display='none'
+        menuHidden=true;
+    }
+    else {
+        primaryMenu = document.style.display='block;'
+        menuHidden=false;
+    }
+});
+
+menuBtn.addEventListener('mouseover', () => {
+    document.body.style.backgroundColor = 'orange';
+});
