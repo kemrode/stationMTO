90817613957ff46fa3a7dd3b38befc7f2d636114
Kevin Fichou <kevinfichou@gmail.com>
2021-02-02 22:21:00 +0100

    <create>303: end of day 02FEB21

--- scriptJavaS.js
+++ scriptJavaS.js
@@ -1,17 +1,22 @@
+// definition of variables about the menu roll out type
 let menuBtn = document.querySelector('button');
 let primaryMenu = document.querySelector('#menuRollOut');
 let menuHidden=true;
 
+// definition of variables about the elements of MTO case
+let temp = document.querySelector('temperatureNumber');
+let humidity = document.querySelector('hygromethrieNumber');
 
-primaryMenu.style.display="none";
+
+// primaryMenu.style.display="none";
 
 menuBtn.addEventListener('click', ()=> {
     if(menuHidden){
         primaryMenu.style.display="block";
+        primaryMenu.style.display='flex';
         menuHidden = false;
     }
     else {
-        
         primaryMenu.style.display="none";
         menuHidden = true;
     }
