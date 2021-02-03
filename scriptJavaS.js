dbf10b30fa61deb62b24fddf17b924193c33329b
Kevin Fichou <kevinfichou@gmail.com>
2021-02-03 15:59:22 +0100

    first commit on new branch with added implementation of real time and date

--- scriptJavaS.js
+++ scriptJavaS.js
@@ -6,23 +6,31 @@ let menuHidden=true;
 // definition of variables about the elements of MTO case
 let temp = document.querySelector('temperatureNumber');
 let humidity = document.querySelector('hygromethrieNumber');
+let date = document.querySelector('.groupDate');
+let hour = document.querySelector('.groupHour');
 let dateOfDay = new Date();
-// toLocaleDateString = jour, mois, année
-// toLocaleTimeString = l'heure
-// toLocaleString = jour, mois, l'année, l'heure
 
-// let dateLocal = dateOfDay.toLocaleString('fr-FR', {
-//     year: 'numeric',
-//     month : 'numeric',
-//     day : 'numeric',
-// });
+let localDate = dateOfDay.toLocaleString(navigator.language, {
+    year : 'numeric',
+    month : 'numeric',
+    day: 'numeric'
+});
 
-// let localTime = dateOfDay.toLocaleString('fr-FR', {
-//     hour : 'numeric';
-//     minute: 'numeric';
-// });
+console.log(date);
+console.log(hour);
+console.log(localDate);
 
-// console.log();
+let localTime = dateOfDay.toLocaleString(navigator.language, {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+});
+
+console.log(localTime);
+
+// functions to set the local time and date
+date.textContent = `${localDate}`;
+hour.textContent = `${localTime}`;
 
 
 primaryMenu.style.display="none";
