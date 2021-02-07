8445f87fcf2f148228dbffb9fe804204cb164838
Kemrode <66632429+kemrode@users.noreply.github.com>
2021-02-07 16:17:44 +0100

    <feat>3031: commit after merged into master

--- scriptJavaS.js
+++ scriptJavaS.js
@@ -1,33 +1,34 @@
 // definition of variables about the menu roll out type
 let menuBtn = document.querySelector('button');
 let primaryMenu = document.querySelector('#menuRollOut');
+// let primaryMenu = document.getElementById('#menuRollOut');
 // let mapped = document.querySelector('#map');
 let menuHidden=true;
 
 // definition of variables about the elements of MTO case
 let temp = document.querySelector('temperatureNumber');
 let humidity = document.querySelector('hygromethrieNumber');
-let date = document.querySelector('.groupDate');
-let hour = document.querySelector('.groupHour');
-let dateOfDay = new Date();
+// let date = document.querySelector('.groupDate');
+// let hour = document.querySelector('.groupHour');
+// let dateOfDay = new Date();
 
-let localDate = dateOfDay.toLocaleString(navigator.language, {
-    year : 'numeric',
-    month : 'numeric',
-    day: 'numeric'
-});
+// let localDate = dateOfDay.toLocaleString(navigator.language, {
+//     year : 'numeric',
+//     month : 'numeric',
+//     day: 'numeric'
+// });
 
-let localTime = dateOfDay.toLocaleString(navigator.language, {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-});
+// let localTime = dateOfDay.toLocaleString(navigator.language, {
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     second: 'numeric'
+// });
 
 
-date.textContent = `${localDate}`;
-let time = hour.textContent = `${localTime}`;
+// date.textContent = `${localDate}`;
+// let time = hour.textContent = `${localTime}`;
 
-// mapped.style.display = "block";
+
 primaryMenu.style.display="none";
 
 menuBtn.addEventListener('click', ()=> {
