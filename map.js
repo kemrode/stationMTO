b306bb338f8651ce4c9841ddf45296e97bb89e9f
Kemrode <66632429+kemrode@users.noreply.github.com>
2021-02-08 09:52:00 +0100

    <feat>3031:z-index and center of primary element in file sign up

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ let positSonde = { lat:49.382452 , lng: 1.075189};
 
 function initMap(){
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 10,
         center: positSonde,
     });
 
