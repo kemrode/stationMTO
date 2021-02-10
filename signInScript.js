bc223428fab7f9af77191b0ccb320199f48536a6
Kemrode <66632429+kemrode@users.noreply.github.com>
2021-02-10 09:42:57 +0100

    <feat>3031: GET on signIn OK

--- signInScript.js
+++ signInScript.js
@@ -1,3 +1,7 @@
+// definition of variables about the menu roll out type
+let menuBtn = document.querySelector('button');
+let primaryMenu = document.querySelector('#menuRollOut');
+let menuHidden=true;
 
 //bring back the script to use jQuery
 var script = document.createElement('jQuery');
@@ -5,16 +9,37 @@ script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-//definition of a constant for URL requests
-const url = 'http://192.168.137.176:5000/API/RASPBERRY/SIGNUP/<string:xLastName>/<string:xFirstName>/<string:xCity>/<string:xEmail>/<string:xPassword>';
+//function to know if the user is logged or not
+// () => {
+//     console.log(connect);
+//     if(connect){
+//         $('surname').text('xLastName');
+//     }
+// }
 
+//function to change display of the menuRollOut
+primaryMenu.style.display="none";
 
+menuBtn.addEventListener('click', ()=> {
+    if(menuHidden){
+        primaryMenu.style.display="block";
+        primaryMenu.style.display='flex';
+        primaryMenu.style.width='8em';
+        primaryMenu.style.zIndex=1;
+        menuHidden = false;
+    }
+    else {
+        primaryMenu.style.display="none";
+        menuHidden = true;
+    }
+});
+
+//variables to sign In
 let submitBtn = document.querySelector('.btn');
 var eMail = document.querySelector('#email');
 var pwd = document.querySelector('#pwd');
 let nameTitle = document.querySelector('.name');
 let surnameTitle = document.querySelector('.surname');
-var connect = false;
 
 let dictSignIn = {
     'xEmail': "",
@@ -34,16 +59,21 @@ eMail.addEventListener('click', ()=> {
     pwd.style.color='black';
 });
 
-function postPasswordAndEmail(dict) {
+function postPasswordAndEmail(dictSignIn) {
     $.ajax({
-        type: "POST",
-        url: `http://192.168.137.176:5000/API/RASPBERRY/SIGNIN/${dict['xEmail']}/${dict['xPassword']}`,
+        type: "GET",
+        url: `http://192.168.137.176:5000/API/RASPBERRY/SIGNIN/${dictSignIn['xEmail']}/${dictSignIn['xPassword']}`,
         success: function (response) {
             console.log("all is working fine !");
-            return connect=true;
+            console.log(response);
+            // let connect=true;
+
+            // if(connect==true){
+            //     getElementsSignUp(response);
+            // }
         },
         error: ()=> {
-            console.log(JSON.stringify(dict));
+            // console.log(JSON.stringify(dict));
             alert("une erreur est survenue lors de l'envoi des données\nveuillez réessayer plus tard, svp.");
         }
     });
@@ -57,18 +87,21 @@ function getElementsSignUp(dict) {
         type: 'GET',
         dataType: 'text',
         success: function(response) {
+            console.log(dict);
             console.log("all receive fine !");
-            connect = true;
             $('PRENOM').text('xLastName');
         },
         error: () => {
-            alert("une erreur est survenue lors de l'envoi des données\nveuillez réessayer plus tard, svp.");
-            connect=false;
+            alert("une erreur est survenue lors de la réception des données\nveuillez réessayer plus tard, svp.");
         }
     });
+    console.log("fin de la fonction getElementsSignUp");
 }
 
 submitBtn.addEventListener('click', ()=> {
+
+    
+    var connect = false;
 
     eMail = document.querySelector('#email');
     pwd = document.querySelector('#pwd');
@@ -76,15 +109,16 @@ submitBtn.addEventListener('click', ()=> {
     dictSignIn['xEmail']=eMail.value;
     dictSignIn['xPassword']=pwd.value;
 
-    console.log(dictSignIn);
+    // console.log(dictSignIn);
     postPasswordAndEmail(dictSignIn);
-    console.log(connect);
-    if(connect){
-        getElementsSignUp(dict);
-    }
-    else {
-        alert("problème détecté");
-    }
-    console.log(dict);
+    connect = true;
+    // console.log(connect);
+    // if(connect){
+    //     getElementsSignUp(dictSignIn);
+    // }
+    // else {
+    //     alert("on ne rentre pas dans la fonction getElementsSignUp");
+    // }
+    console.log(dictSignIn);
 });
 
