97cd7f6449f6367b1b0a899e8807f525b11fb855
Kemrode <66632429+kemrode@users.noreply.github.com>
2021-02-10 14:15:37 +0100

    <feat>3031:media queries history.css OK except one

---
+++ history.js
@@ -0,0 +1,48 @@
+let userName = document.querySelector('.surname');
+
+// definition of variables about the menu roll out type
+let menuBtn = document.querySelector('button');
+let primaryMenu = document.querySelector('#menuRollOut');
+let menuHidden=true;
+
+// definition of variables about the elements of MTO case
+let temp = document.querySelector('temperatureNumber');
+let humidity = document.querySelector('hygromethrieNumber');
+
+//bring back the script to use jQuery
+var script = document.createElement('jQuery');
+script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+if(localStorage.getItem('userName')){
+    userName.textContent = localStorage.getItem('userName');
+}
+
+//function to know if the user is logged or not
+() => {
+    console.log(connect);
+    if(connect){
+        $('surname').text('xLastName');
+        $('name').text('xFirstName');
+    }
+}
+
+
+//function to change display of the menuRollOut
+primaryMenu.style.display="none";
+
+menuBtn.addEventListener('click', ()=> {
+    if(menuHidden){
+        primaryMenu.style.display="block";
+        primaryMenu.style.display='flex';
+        primaryMenu.style.width='8em';
+        primaryMenu.style.zIndex=1;
+        menuHidden = false;
+    }
+    else {
+        primaryMenu.style.display="none";
+        menuHidden = true;
+    }
+});
+
